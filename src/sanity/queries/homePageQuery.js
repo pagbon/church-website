9910bc94c74ed5d4f573c2fca8ae81a44717288d
pagbon <pagbongiator@gmail.com>
2025-11-22 08:04:39 -0700

    Update homePageQuery.js

--- src/sanity/queries/homePageQuery.js
+++ src/sanity/queries/homePageQuery.js
@@ -1,16 +1,16 @@
 export const homePageQuery = `
-  *[_type == "homePage"][0]{
-    title,
-    sections[]{
-      ...,
-      _type,
-      image{asset->{url}, alt},
-      topBannerImage{asset->{url}, alt},
-      slides[]{asset->{url}, alt},
-      images[]{asset->{url}, alt},
-      icon{asset->{url}, alt},
-      thumbnail{asset->{url}, alt}
-    }
+*[_type == "homeType"][0]{
+  title,
+  sections[] {
+    ...,
+    _type,
+    image{asset->{url}, alt},
+    topBannerImage{asset->{url}, alt},
+    slides[]{asset->{url}, alt},
+    images[]{asset->{url}, alt},
+    icon{asset->{url}, alt},
+    thumbnail{asset->{url}, alt}
   }
+}
 `;
 
