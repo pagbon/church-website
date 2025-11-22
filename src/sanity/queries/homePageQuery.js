5f4ca67af3d5f00e334e351188c57920f4873b9e
pagbon <pagbongiator@gmail.com>
2025-11-22 03:19:51 -0700

    Update homePageQuery.js

--- src/sanity/queries/homePageQuery.js
+++ src/sanity/queries/homePageQuery.js
@@ -2,45 +2,18 @@ export const homePageQuery = `
   *[_type == "homePage"][0]{
     title,
     sections[]{
+      ...,
       _type,
-      topBannerImage{
-        asset->{ url }
-      },
-      heroText,
-      subtitle,
-      ctaText,
-      ctaRoute,
-      slides[]{
-        asset->{url},
-        alt
-      },
-      images[]{
-        asset->{url},
-        alt
-      },
-      items[]{
-        title,
-        description,
-        linkText,
-        linkUrl
-      },
-      cards[]{
-        icon{ asset->{url}, alt },
-        title,
-        description
-      },
-      body[]{ ..., children[]{ ..., marks[] } },
-      intro[]{ ..., children[]{ ..., marks[] } },
-      heading,
-      image{ asset->{url}, alt },
-      paragraphs,
-      description,
-      title,
-      videoUrl,
-      isLive,
-      thumbnail{ asset->{url}, alt },
-      date,
-      description
+
+      // Expand images for all section types
+      image{asset->{url}, alt},
+      topBannerImage{asset->{url}, alt},
+
+      slides[]{asset->{url}, alt},
+      images[]{asset->{url}, alt},
+
+      icon{asset->{url}, alt},
+      thumbnail{asset->{url}, alt}
     }
   }
 `;
