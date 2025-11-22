f489bd6d8b71c72498406cf879194b7550d177b7
pagbon <pagbongiator@gmail.com>
2025-11-22 08:49:02 -0700

    Update sanityClient.js

--- src/sanity/sanityClient.js
+++ src/sanity/sanityClient.js
@@ -4,6 +4,12 @@ export const client = createClient({
   projectId: "k06kgk7u",
   dataset: "production",
   useCdn: true,
-  apiVersion: "2025-11-22",
+  apiVersion: "2023-10-01",     
 });
+
+export async function getHomePage() {
+  const query = `*[_type == "homePage"][0]`;  
+  return await client.fetch(query);
+}
+
 
